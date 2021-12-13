1671f0399181313948e7628453ede7b5e8b744df
Natália Nascimento <natalia.nascc@gmail.com>
2021-12-13 17:29:40 -0300

    aqui coloco uma mensagem, tipo: comitando primeiro post

---
+++ View/Cursos/formulario.php
@@ -0,0 +1,11 @@
+<?php include __DIR__ .'/../inicioHtml.php'; ?>
+
+<form action="">
+    <div class="form-group">
+        <label for="descricao">Descrição</label>
+        <input type="text" id="descricao" name="descricao" class="form-control">
+    </div>
+    <button class="btn btn-primary">Salvar</button>
+</form>
+
+<?php include __DIR__ .'/../fimHtml.php'; ?>
---
+++ src/Controller/ListarCursos.php
@@ -0,0 +1,23 @@
+<?php
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+Class ListarCursos implements InterfaceControladorRequisicao
+{
+    private $repositorioDeCursos;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())->getEntityManager();
+        $this->repositorioDeCursos = $entityManager->getRepository(Curso::class);
+    }
+
+    public function processaRequisicao() :void
+    {
+        $cursos = $this->repositorioDeCursos->findAll();
+        $titulo = 'Lista de Cursos';
+        require __DIR__ . '/../../View/Cursos/listar-cursos.php';
+    }
+}
